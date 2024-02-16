91fdd0ab49e0b5a47b77422ac21c55283f86f708
rdubois <remi.dubois@imio.be>
2024-02-16 14:48:55 +0100

    Back to development: 1.0a3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.plausible",
-    version="1.0a2",
+    version="1.0a3.dev0",
     description="Plausible integration into Plone 5 and Plone 6 classic UI",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
